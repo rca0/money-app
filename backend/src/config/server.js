7252d616a36519ce975c55e12997a60e51ffa046
Ruan Arcega <ruanarcega@gmail.com>
2019-11-19 01:50:52 -0300

    feat: configure middleware query parser int

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser') // handle requests
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`Server is running on port ${port}.`)
