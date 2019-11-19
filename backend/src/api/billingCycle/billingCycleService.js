066ef1261674a0d0bca6951a47fb4f65a882cd79
Ruan Arcega <ruanarcega@gmail.com>
2019-11-19 01:03:43 -0300

    feat: get number of records

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,16 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 // execute validators when execute updates
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count', (req, res, next) => {
+    // mongoose method already exist
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            // return the count of elements
+            res.json({ value })
+        }
+    })
+})
+
 module.exports = BillingCycle
