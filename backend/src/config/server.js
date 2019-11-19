e534af1f0d566b434764363cfc4aa112227a5929
Ruan Arcega <ruanarcega@gmail.com>
2019-11-18 23:57:50 -0300

    feat: configure express server

---
+++ backend/src/config/server.js
@@ -0,0 +1,12 @@
+const port = 3000
+
+const bodyParser = require('body-parser') // handle requests
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`Server is running on port ${port}.`)
+})
