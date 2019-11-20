04253db76713ae311f96418764e6bf03cf5c53d3
Ruan Arcega <ruanarcega@gmail.com>
2019-11-20 00:55:56 -0300

    feat: add menu component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,11 @@ import '../common/template/dependecies'
 import React from 'react'
 
 import Header from '../common/template/header'
+import SideBar from '../common/template/sidebar'
 
 export default props => (
     <div className='wrapper'>
         <Header />
+        <SideBar />
     </div>
 )
