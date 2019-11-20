c66a52d185fd35bc4232e4e8f2f621232d1bef98
Ruan Arcega <ruanarcega@gmail.com>
2019-11-20 01:58:52 -0300

    feat: configure valueBox in dashboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +11,14 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='v1.0' />
                 <Content>
-                    Dashboard content
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value='R$ 10,00' text='Total Credits' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value='R$ 20,00' text='Total Debts' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value='R$ 0' text='Consolidated value' />
+                    </Row>
                 </Content>
             </div>
         )
