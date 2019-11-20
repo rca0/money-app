c5743ec3936ffbee1b4529fee8276723bc7ba96f
Ruan Arcega <ruanarcega@gmail.com>
2019-11-20 01:14:33 -0300

    feat: add footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='https://www.arcega.com.br' target='_blank'> Arcega</a>
+        </strong>
+    </footer>
+)
