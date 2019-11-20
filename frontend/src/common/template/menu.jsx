d8cda477390bbb3b8cf8ee8827d10a1e85975844
Ruan Arcega <ruanarcega@gmail.com>
2019-11-20 01:22:51 -0300

    feat: navigation with react-router

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -6,7 +6,7 @@ export default props => (
     <ul className='sidebar-menu'>
         <MenuItem path='#' label='Dashboard' icon='dashboard' />
         <MenuTree label='Register' icon='edit'>
-            <MenuItem path='#billingCycles'
+            <MenuItem path='#billingCycle'
                 label='Payment Cycles' icon='usd' />
         </MenuTree>
     </ul>
