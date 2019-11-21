ccdf69566d930aaa8a9b41ec23b4d2b313daa186
Ruan Arcega <ruanarcega@gmail.com>
2019-11-21 00:25:57 -0300

    feat: configure tabs in billing cycle component

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 
-import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-import contentHeader from '../common/template/contentHeader'
+import ContentHeader from '../common/template/contentHeader'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
 
 class BillingCycle extends Component {
     render() {
         return (
             <div>
-                <contentHeader title='Payment Cycles' small='Register' />
+                <ContentHeader title='Payment Cycles' small='Register' />
                 <Content>
-                    Payment Cycles
+                    <Tabs>
+                        <TabsHeader>
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
