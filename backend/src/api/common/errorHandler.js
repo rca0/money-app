adf160f3d01b32b710fa65fffcb04c03e7e3adce
Ruan Arcega <ruanarcega@gmail.com>
2019-11-23 01:59:26 -0300

    fix: missing error object

--- backend/src/api/common/errorHandler.js
+++ backend/src/api/common/errorHandler.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
 
     if(bundle.errors) {
         const errors = parseErrors(bundle.errors)
-        res.status(500).json({ errors })
+        res.status(500).json({errors})
     } else {
         next()
     }
@@ -14,6 +14,6 @@ module.exports = (req, res, next) => {
 
 const parseErrors = (nodeRestfulErrors) => {
     const errors = []
-    _.forIn(nodeRestfulErrors, error => errors.push())
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
     return errors
 }
