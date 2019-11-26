10ab0cd11ba549cba9c09592925ecf8ba07841e7
Ruan Arcega <ruanarcega@gmail.com>
2019-11-26 13:56:20 -0300

    feat: improve form buttons

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -39,13 +39,23 @@ class BillingCycle extends Component {
                                 <List />
                             </TabContent>
                             <TabContent id='tabCreate'>
-                                <Form onSubmit={this.props.create} />
+                                <Form
+                                onSubmit={this.props.create}
+                                submitLabel='Insert'
+                                submitClass='primary' />
                             </TabContent>
                             <TabContent id='tabUpdate'>
-                                <Form onSubmit={this.props.update} />
+                                <Form
+                                onSubmit={this.props.update}
+                                submitLabel='Update'
+                                submitClass='warning' />
                             </TabContent>
                             <TabContent id='tabDelete'>
-                                <Form onSubmit={this.props.remove} readOnly={true} />
+                                <Form
+                                    onSubmit={this.props.remove}
+                                    readOnly={true}
+                                    submitLabel='Delete'
+                                    submitClass='danger' />
                             </TabContent>
                         </TabsContent>
                     </Tabs>
