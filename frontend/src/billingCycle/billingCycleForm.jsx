bc0f1323322fe5469e2bfd9e5197a297c1d2ab8b
Ruan Arcega <ruanarcega@gmail.com>
2019-12-01 23:05:27 -0300

    feat: add creditList basic component

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field } from 'redux-form'
 
 import { init } from '../billingCycle/billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput'
-
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
     render() {
@@ -20,6 +20,7 @@ class BillingCycleForm extends Component {
                         type='number' label='Month' placeholder='Type month' />
                     <Field name='year' component={labelAndInput} cols='12 4' readOnly={readOnly}
                         type='number' label='Year' placeholder='Type year' />
+                    <CreditList cols='12 6' readOnly={readOnly} />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
---
+++ frontend/src/billingCycle/creditList.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Grid from '../common/layout/grid'
+import Field from 'redux-form'
+import Input from '../common/form/input'
+
+export default class CreditList extends Component {
+
+    renderRows() {
+        return (
+            <tr>
+                <td><Field name='credits[0].name' component={Input}
+                    placeholder='Add name' readOnly={this.props.readOnly} /></td>
+                <td><Field name='credits[0].value' component={Input}
+                    placeholder='Add value' readOnly={this.props.readOnly} /></td>
+                <td></td>
+            </tr>
+        )
+    }
+
+    render() {
+        return (
+            <Grid cols={this.props.cols}>
+                <fieldset>
+                    <legend>Credits</legend>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Value</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>{this.renderRows()}</tbody>
+                    </table>
+                </fieldset>
+            </Grid>
+        )
+    }
+}
