de0a25e5c5e54d85af24648c7248cb3b030dcd5f
Ruan Arcega <ruanarcega@gmail.com>
2019-12-03 01:54:25 -0300

    feat: add auth Actions

---
+++ frontend/src/consts.js
@@ -0,0 +1,4 @@
+export default {
+    API_URL: 'http://localhost:3000/v1/api',
+    OAPI_URL: 'http://loocalhost:3000/v1/oapi'
+}
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -5,8 +5,10 @@ import { reducer as toastrReducer } from 'react-redux-toastr'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import AuthReducer from '../auth/authReducer'
 
 const rootReducer = combineReducers({
+    auth: AuthReducer,
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
